1924eaa6bc891c50f7d219644b0222ad88583451
Gangabathina Chanakya <chanakyagangabathina77@gmail.com>
2026-08-06 11:46:06 +0530

    feat: add task model and create task endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
 app = FastAPI()
+from app.models.task import Task
 
 @app.get("/")
 def root():
     return {
         "message" : "Welcome to Taskflow AI Backend!"
+    }
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id:int):
+    return {
+        "task_id":task_id,
+        "message":f"Fetching task {task_id}"
+    }
+
+@app.get("/search")
+def search_tasks(priority: str, completed : bool):
+    return{
+        "priority": priority,
+        "completed": completed,
+    }
+
+@app.post("/tasks")
+def create_task(task: Task):
+    return {
+        "message": "Task created successfully",
+        "task": task
     }
